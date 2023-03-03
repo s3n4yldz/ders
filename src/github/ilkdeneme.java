aa6b320c826fa592c8be268a2fd051a57818882f
Sena Nur Yildiz <yildiznur497@gmail.com>
2023-03-03 19:59:16 +0100

    v4#

--- src/github/ilkdeneme.java
+++ src/github/ilkdeneme.java
@@ -4,4 +4,9 @@ public class ilkdeneme {
 // 1. adim yaziyorum
 //2. adim olustu
 //3. adima gectik
+//4. adimada gectik
+	
+//git log, git log --oneline: version olusturulma bilgileri gözükür
+//git status: durumu gösterir
+//git show+" "+versionlarin ilk 7 cod: versionlardaki degisiklikleri gösterir 
 }
